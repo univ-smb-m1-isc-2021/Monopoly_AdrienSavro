57c1dd8d3b9b1d26d733b89736754bb839311cdf
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-01-03 17:20:48 +0100

    pushv2

--- src/main/java/calc/lanceur1.java
+++
@@ -1,9 +0,0 @@
-package calc;
-
-import java.io.IOException;
-
-public class lanceur1 {
-	public static void main(String[] arg) throws IOException{
-		Calculatrice calc = new Calculatrice(new MoteurCalculSimple());
-	}
-}
