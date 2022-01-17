e247d1fe07c264aa81067fa607ff97615aaacefe
Savro01 <59883445+Savro01@users.noreply.github.com>
2022-01-17 16:33:29 +0100

    Test unitaire

--- src/test/java/CaseProprieteTest.java
+++ src/test/java/CaseProprieteTest.java
@@ -19,35 +19,6 @@ class CaseProprieteTest {
 	CasePropriete marron2 = new CasePropriete("Rue Lecourbe", 60, 4, 20, 60, 180, 320, 450);
 	
 	Joueur tom = new Joueur("Tom", marron1, null); // L'argent de Tom est initialisé à 5000 €
-
-	@Test
-	void testEstProprietaire() {
-		
-		marron1.majEtat(new EtatAchete(marron1));
-		
-		marron1.operationPossible(tom);
-		
-		System.out.println(marron1.etat);
-		
-		assertEquals(marron1.etat.getClass(), EtatLibre.class);
-	}
-	
-	
-	@Test
-	void testAjouterMaison() {
-		
-		marron1.setProprietaire(tom);
-		tom.ajoutePropriete(marron1);
-		
-		tom.setArgent(100);
-
-		
-		boolean peut = marron1.ajouterMaison(1);
-		
-		assertTrue(peut);
-		
-		
-	}
 	
 	@Test
 	void testDonneLoyerConstructible() {
